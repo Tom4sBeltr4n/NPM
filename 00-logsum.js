f2efe294f37dae7410fb9a099e5225ce8b7f85a2
Tom4sBeltr4n <tomasfelipe29@hotmail.com>
2022-07-21 20:44:26 -0500

    First class finished, added basic starting files

---
+++ 00-logsum.js
@@ -0,0 +1,8 @@
+/*
+https://platzi.com/cursos/npm/
+
+1. Acerca de NPM
+
+NPM significa Node Package Manager y permite descargar, subir y conocer "paquetes". Estos paquetes son módulos y librerías de JS que sirven para dar característcas a proyectos. Esto ahorra tiempo a un programador, porque no tiene que hacer todo de cero cada vez que trabaja. Tiene un sitio web, donde se puede conocer la información de cada paquete (colaboradores, versión actual, descargas, etc.)
+
+*/
